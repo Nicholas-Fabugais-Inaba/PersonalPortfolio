d774448129be4407ccc5e2aad6a13eca20c6d648
Nicholas-Fabugais-Inaba <71365004+Nicholas-Fabugais-Inaba@users.noreply.github.com>
2026-02-01 22:18:20 -0500

    Updated vite.config.ts

--- src/components/Trips.tsx
+++ src/components/Trips.tsx
@@ -14,7 +14,7 @@ interface Trip {
 
 // Dynamically import all gallery images from public folder using Vite's glob
 const globImages = import.meta.glob<{ default: string }>(
-  '/public/*/[!.]*.{jpg,jpeg,png}',
+  '/public/*/[!.]*',
   { eager: true }
 );
 
@@ -24,8 +24,14 @@ const buildGalleryImages = () => {
 
   Object.entries(globImages).forEach(([path, module]) => {
     const folderName = path.split('/')[2];
-    if (!galleries[folderName]) galleries[folderName] = [];
-    galleries[folderName].push(module.default);
+    const fileName = path.split('/')[3];
+    const imageExtensions = ['.jpg', '.jpeg', '.png', '.JPG', '.JPEG', '.PNG'];
+    const hasValidExtension = imageExtensions.some(ext => fileName?.toLowerCase().endsWith(ext.toLowerCase()));
+
+    if (hasValidExtension) {
+      if (!galleries[folderName]) galleries[folderName] = [];
+      galleries[folderName].push(module.default);
+    }
   });
 
   return galleries;
